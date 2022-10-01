ce23c69d2979e312b7580ccbd4d357150a0cc3c5
Tony Narlock <tony@git-pull.com>
2022-10-01 14:24:19 -0500

    Tag v0.0.1a15 (warning fix from #15)

--- src/gp_libs.py
+++ src/gp_libs.py
@@ -1,7 +1,7 @@
 __title__ = "gp-libs"
 __package_name__ = "gp_libs"
 __description__ = "Internal utilities for git-pull projects"
-__version__ = "0.0.1a14"
+__version__ = "0.0.1a15"
 __author__ = "Tony Narlock"
 __github__ = "https://github.com/git-pull/gp-libs"
 __docs__ = "https://gp-libs.git-pull.com"
