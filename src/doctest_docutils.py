acd3e744c652009e1078f02e0e41bb2496c0e0a3
Tony Narlock <tony@git-pull.com>
2022-10-01 11:27:10 -0500

    fix(docutils): Move findall -> traverse (docutils 0.19+)

--- src/doctest_docutils.py
+++ src/doctest_docutils.py
@@ -325,7 +325,7 @@ class DocutilsDocTestFinder:
                 or isinstance(node, nodes.doctest_block)
             )
 
-        for idx, node in enumerate(doc.findall(condition)):
+        for idx, node in enumerate(doc.traverse(condition)):
             logger.debug(f"() node: {node.astext()}")
             assert isinstance(node, nodes.Element)
             test_name = node.get("groups")
